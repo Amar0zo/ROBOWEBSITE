234eaeef4b2add99958952edb0b7cda671e90aed
Amarozoo <marek537@onet.pl>
2023-04-03 21:59:45 +0200

    robot

--- website/myapp/robot.py
+++ website/myapp/robot.py
@@ -1,32 +1,48 @@
 import socket
 
 class Robot:
-    def __init__(self, ip_address, port):
-        self.ip_address = ip_address
+    def __init__(self, port=29999):
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connected = False
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self):
+    def connect(self, ip_address):
         try:
-            self.sock.connect((self.ip_address, self.port))
+            self.sock.connect((ip_address, self.port))
             self.connected = True
             data = self.sock.recv(100)
             print('received "%s"' % data)
-
+            return True
         except:
-            pass
+            return False       
 
     def send_command(self, command):
         try:
             message = command + '\n'
             print('sending "%s"' % message)
             self.sock.sendto(message.encode(), (self.ip_address, self.port))
-            data = self.sock.recv(100)
-            print('received "%s"' % data)
-
+            response = str(self.sock.recv(100))
+            print('received "%s"' % response)
+            return True, response
         except:
-            pass
+            response=""
+            return False, response
+
+    def power_on (self):
+        print ("Power ON")
+        (status, response) = self.send_command ("power on")
+        print (str(status), response)
+        if status:
+            if "Powering on" in response:
+                print ("OK")
+                return True
+            else:
+                print ("Error")
+                return False
+        else:
+            return False
+
+
 
     def disconnect(self):
         self.sock.close()
