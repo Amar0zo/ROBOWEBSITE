68438b6707ded93dd445b05eb6bd8c20749d580a
kneicik <knajcikpl@gmail.com>
2023-04-21 19:56:43 +0200

    1

--- website/static/js/script_more.js
+++ website/static/js/script_more.js
@@ -12,3 +12,129 @@ fetch('http://'+ip+'/api/connection',{
     console.log('printer name:', data);
     document.getElementById("printer-name").innerHTML = 'Drukarka - więcej (' + data.current.printerProfile +')';
     });
+
+document.getElementById('xplus').addEventListener('click', function() {
+    fetch('http://'+ip+'/api/printer/command', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': api
+    },
+    body: JSON.stringify({
+        apikey: api,
+      command: [
+        'G0 X+10',
+      ]
+          
+    })
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Odpowiedź:', data);
+    });
+    });
+    
+    document.getElementById('xmin').addEventListener('click', function() {
+    fetch('http://'+ip+'/api/printer/command', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': api
+    },
+    body: JSON.stringify({
+        apikey: api,
+      command: [
+        'G0 X-10',
+      ]
+          
+    })
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Odpowiedź:', data);
+    });
+    });
+        
+    document.getElementById('yplus').addEventListener('click', function() {
+    fetch('http://'+ip+'/api/printer/command', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': api
+    },
+    body: JSON.stringify({
+        apikey: api,
+      command: [
+        'G0 Y+10',
+      ]
+          
+    })
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Odpowiedź:', data);
+    });
+    });
+        
+    document.getElementById('ymin').addEventListener('click', function() {
+    fetch('http://'+ip+'/api/printer/command', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': api
+    },
+    body: JSON.stringify({
+        apikey: api,
+      command: [
+        'G0 Y-10',
+      ]
+          
+    })
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Odpowiedź:', data);
+    });
+    });
+        
+    document.getElementById('zplus').addEventListener('click', function() {
+    fetch('http://'+ip+'/api/printer/command', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': api
+    },
+    body: JSON.stringify({
+        apikey: api,
+      command: [
+        'G0 Z+10',
+      ]
+          
+    })
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Odpowiedź:', data);
+    });
+    });
+        
+    document.getElementById('zmin').addEventListener('click', function() {
+    fetch('http://'+ip+'/api/printer/command', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': api
+    },
+    body: JSON.stringify({
+        apikey: api,
+      command: [
+        'G0 Z-10',
+      ]
+          
+    })
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Odpowiedź:', data);
+    });
+    });
